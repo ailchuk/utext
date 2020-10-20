c17abfc6429aca76e33168303912c50317fcb0af
ronald112 <ronald11211@gmail.com>
2020-10-20 15:58:01 +0300

    add Highlight while typing

--- app/src/utils.cpp
+++ app/src/utils.cpp
@@ -29,27 +29,24 @@ void MainWindow::showCursorPos()
     QTextCursor tc = m_ui->textEdit->textCursor();
     tc.select(QTextCursor::SelectionType::WordUnderCursor);
     std::cerr << tc.selectedText().toStdString().size() << std::endl;
-    if (tc.anchor() != tc.position()) { // true only if there is some symbols under cursor
-        cursor.clearSelection();
-        std::cerr << tc.anchor() << " " << tc.position() << std::endl;
-        tc.movePosition(QTextCursor::StartOfWord);
-        tc.movePosition(QTextCursor::EndOfWord, QTextCursor::KeepAnchor);
+    if (tc.selectedText().toStdString().size() == 0) {
+        m_ui->textEdit->selections.clear();
+        m_ui->textEdit->setExtraSelections(m_ui->textEdit->selections);
+    }
+    else {
+        //std::cerr << tc.anchor() << " " << tc.position() << std::endl;
+        //tc.movePosition(QTextCursor::StartOfWord);
+        //tc.movePosition(QTextCursor::EndOfWord, QTextCursor::KeepAnchor);
         std::cerr << tc.selectedText().toStdString() << std::endl;
 
         MyTextEdit::ExtraSelection sel;
 
         QTextEdit::ExtraSelection selection;
+
         selection.format.setBackground(Qt::green);
-        selection.format.setProperty(QTextFormat::FullWidthSelection, true);
         selection.cursor = tc;
-//        selection.cursor.clearSelection();
         m_ui->textEdit->selections.append(selection);
         m_ui->textEdit->setExtraSelections(m_ui->textEdit->selections);
 
-//        while( !(tc = m_ui->textEdit->document()->find(QRegExp("\\([^)]*\\)"), tc)).isNull()) {
-//           MyTextEdit::ExtraSelection sel = { tc, m_ui->textEdit->fmt };
-//           m_ui->textEdit->selections.append(sel);
-//        }
-        //m_ui->textEdit->setExtraSelections(m_ui->textEdit->selections);
     }
 }
